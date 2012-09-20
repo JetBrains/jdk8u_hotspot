323b03ab73d7e79666bc7bc39c3842576f2dc074
aph <none@none>
2012-09-20 16:38:25 +0100

    Remove debud code

--- src/share/vm/interpreter/interpreterRuntime.cpp
+++ src/share/vm/interpreter/interpreterRuntime.cpp
@@ -677,12 +677,8 @@ IRT_ENTRY(void, InterpreterRuntime::_breakpoint(JavaThread* thread, methodOopDes
   JvmtiExport::post_raw_breakpoint(thread, method, bcp);
 IRT_END
 
-static long foo;
-
 IRT_ENTRY(void, InterpreterRuntime::resolve_invoke(JavaThread* thread, Bytecodes::Code bytecode))
-foo++;
   // extract receiver from the outgoing argument list if necessary
-
   Handle receiver(thread, NULL);
   if (bytecode == Bytecodes::_invokevirtual || bytecode == Bytecodes::_invokeinterface) {
     ResourceMark rm(thread);
