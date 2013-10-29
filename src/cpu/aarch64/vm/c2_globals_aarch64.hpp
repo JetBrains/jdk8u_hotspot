39377a430836f49b4deaeec65e8bbb60d69241ea
adinn <none@none>
2013-10-29 15:49:11 +0000

    corrected register match rules, ensured rule outputs are all iRegXNoSp
    the iRegPXXX operand declarations were matching registers the wrong
    way round and as a consequence were allowing matches which were
    unnecessary. they are now correct and this avoids unnecessary work in
    the generated DFA code.
    
    more seriously, some of the rules were declaring their outputs with
    type iRegX instead of iRegXNoSp i.e. allowing the allocator to
    allocate the destination output from the special register set R27 to
    R31. this results in an asser in the register pressure computation in
    build_ifg_physical. specifically, this happens when a which rule
    generates an output declared with type iRegX produces a live range
    which crosses a VM callout. the fat projection following the callout
    kills the SOC registers but does not remove the special registers from
    the range, leaving the range still live even when it ought to have
    been yanked. this is caught by the assert because the register
    pressure for the block gets decremented by the amount associated with
    the yanked projection and it does not tally with the count associated
    with the live ranges.

--- src/cpu/aarch64/vm/c2_globals_aarch64.hpp
+++ src/cpu/aarch64/vm/c2_globals_aarch64.hpp
@@ -52,10 +52,10 @@ define_pd_global(intx, BackEdgeThreshold,            100000);
 
 define_pd_global(intx, OnStackReplacePercentage,     140);
 define_pd_global(intx, ConditionalMoveLimit,         3);
-define_pd_global(intx, FLOATPRESSURE,                30);
+define_pd_global(intx, FLOATPRESSURE,                64);
 define_pd_global(intx, FreqInlineSize,               325);
 define_pd_global(intx, MinJumpTableSize,             10);
-define_pd_global(intx, INTPRESSURE,                  23);
+define_pd_global(intx, INTPRESSURE,                  25);
 define_pd_global(intx, InteriorEntryAlignment,       16);
 define_pd_global(intx, NewSizeThreadIncrease, ScaleForWordSize(4*K));
 define_pd_global(intx, LoopUnrollLimit,              60);
