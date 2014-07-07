76e9061b6db435478509eae235ed1189bb0e2373
Edward Nevill edward.nevill@linaro.org <Edward Nevill edward.nevill@linaro.org>
2014-07-07 16:24:51 +0100

    Add support for MathExact intrinsics

--- test/compiler/intrinsics/mathexact/sanity/IntrinsicBase.java
+++ test/compiler/intrinsics/mathexact/sanity/IntrinsicBase.java
@@ -128,7 +128,7 @@ public abstract class IntrinsicBase extends CompilerWhiteBoxTest {
 
         @Override
         protected boolean isIntrinsicSupported() {
-            return isServerVM() && Boolean.valueOf(useMathExactIntrinsics) && (Platform.isX86() || Platform.isX64());
+            return isServerVM() && Boolean.valueOf(useMathExactIntrinsics) && (Platform.isX86() || Platform.isX64() ||  Platform.isAArch64());
         }
 
         @Override
@@ -144,7 +144,7 @@ public abstract class IntrinsicBase extends CompilerWhiteBoxTest {
 
         @Override
         protected boolean isIntrinsicSupported() {
-            return isServerVM() && Boolean.valueOf(useMathExactIntrinsics) && Platform.isX64();
+            return isServerVM() && Boolean.valueOf(useMathExactIntrinsics) && (Platform.isX64() || Platform.isAArch64());
         }
 
         @Override
