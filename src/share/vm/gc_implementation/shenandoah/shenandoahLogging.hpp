b8750a013bf1f7347e8f92c579a10f8f76b40fb9
rkennke <none@none>
2016-12-08 17:48:03 +0100

    Added dummy arg consumer to pseudo-logging code to be able to build release.

--- src/share/vm/gc_implementation/shenandoah/shenandoahLogging.hpp
+++ src/share/vm/gc_implementation/shenandoah/shenandoahLogging.hpp
@@ -8,6 +8,7 @@
 #define log_develop_trace(...) if (ShenandoahLogTrace) gclog_or_tty->print_cr
 #define log_develop_debug(...) if (ShenandoahLogDebug) gclog_or_tty->print_cr
 #else
-#define log_develop_trace(...)
-#define log_develop_debug(...)
+#define DUMMY_ARGUMENT_CONSUMER(...)
+#define log_develop_trace(...) DUMMY_ARGUMENT_CONSUMER
+#define log_develop_debug(...) DUMMY_ARGUMENT_CONSUMER
 #endif
