0485dbcd84baa09a0a5d1f59bcbc0b7528f506ef
rkennke <none@none>
2017-01-04 13:09:48 +0100

    Fix MXBean Full GC notifications.

--- src/share/vm/services/memoryService.cpp
+++ src/share/vm/services/memoryService.cpp
@@ -198,8 +198,12 @@ void MemoryService::add_g1_heap_info(G1CollectedHeap* g1h) {
 
 void MemoryService::add_shenandoah_heap_info(ShenandoahHeap* pgch) {
   assert(UseShenandoahGC, "sanity");
-  _major_gc_manager = MemoryManager::get_shenandoah_memory_manager();
-  _minor_gc_manager = MemoryManager::get_shenandoah_memory_manager();
+
+  // Need to have different names for these managers, because having the same name
+  // would confuse notification mechanics: it will enable notifications only for
+  // the first manager with the matching name.
+  _major_gc_manager = MemoryManager::get_shenandoah_major_memory_manager();
+  _minor_gc_manager = MemoryManager::get_shenandoah_minor_memory_manager();
   _managers_list->append(_major_gc_manager);
   _managers_list->append(_minor_gc_manager);
   add_shenandoah_memory_pool(pgch, _minor_gc_manager, true);
