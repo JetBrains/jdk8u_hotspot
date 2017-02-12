93bb303f6fa4418392a6d18a886514825ef21e2a
rkennke <none@none>
2017-02-12 14:27:32 +0100

    Fix broken merge: added missing method in connode.cpp

--- src/share/vm/opto/connode.cpp
+++ src/share/vm/opto/connode.cpp
@@ -431,6 +431,24 @@ Node *ConstraintCastNode::Ideal(PhaseGVN *phase, bool can_reshape){
   return (in(0) && remove_dead_region(phase, can_reshape)) ? this : NULL;
 }
 
+//------------------------------Ideal_DU_postCCP-------------------------------
+
+// Throw away cast after constant propagation
+
+Node *ConstraintCastNode::Ideal_DU_postCCP( PhaseCCP *ccp ) {
+
+  const Type *t = ccp->type(in(1));
+
+  ccp->hash_delete(this);
+
+  set_type(t);                   // Turn into ID function
+
+  ccp->hash_insert(this);
+
+  return this;
+
+}
+
 uint CastIINode::size_of() const {
   return sizeof(*this);
 }
