be9fe0efae5193bcc50c1817902cb28361342ed3
shade <none@none>
2018-06-26 18:43:53 +0200

    [backport] Make in-cset checks use signed bytes to match C2 better

--- src/share/vm/opto/shenandoahSupport.cpp
+++ src/share/vm/opto/shenandoahSupport.cpp
@@ -3628,7 +3628,7 @@ void PhaseIdealLoop::shenandoah_in_cset_fast_test(Node*& c, Node* rbtrue, Node* 
     uint in_cset_fast_test_idx = Compile::AliasIdxRaw;
     const TypePtr* in_cset_fast_test_adr_type = NULL; // debug-mode-only argument
     debug_only(in_cset_fast_test_adr_type = C->get_adr_type(in_cset_fast_test_idx));
-    Node* in_cset_fast_test_load = new (C) LoadUBNode(c, raw_mem, in_cset_fast_test_adr, in_cset_fast_test_adr_type, TypeInt::BOOL, MemNode::unordered);
+    Node* in_cset_fast_test_load = new (C) LoadBNode(c, raw_mem, in_cset_fast_test_adr, in_cset_fast_test_adr_type, TypeInt::BOOL, MemNode::unordered);
     register_new_node(in_cset_fast_test_load, c);
     Node* in_cset_fast_test_cmp = new (C) CmpINode(in_cset_fast_test_load, _igvn.zerocon(T_INT));
     register_new_node(in_cset_fast_test_cmp, c);
