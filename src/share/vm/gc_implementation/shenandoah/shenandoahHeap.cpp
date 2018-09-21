cb1fc40cd027db3b4d75118d561a0a78c9992e29
shade <none@none>
2018-09-21 16:26:03 +0200

    [backport] Remove unused ShenandoahEvacuateRootsClosure

--- src/share/vm/gc_implementation/shenandoah/shenandoahHeap.cpp
+++ src/share/vm/gc_implementation/shenandoah/shenandoahHeap.cpp
@@ -769,40 +769,6 @@ public:
   }
 };
 
-class ShenandoahEvacuateRootsClosure: public ExtendedOopClosure {
-private:
-  ShenandoahHeap* _heap;
-  Thread* _thread;
-public:
-  ShenandoahEvacuateRootsClosure() :
-          _heap(ShenandoahHeap::heap()), _thread(Thread::current()) {
-  }
-
-private:
-  template <class T>
-  void do_oop_work(T* p) {
-    T o = oopDesc::load_heap_oop(p);
-    if (! oopDesc::is_null(o)) {
-      oop obj = oopDesc::decode_heap_oop_not_null(o);
-      if (_heap->in_collection_set(obj)) {
-        oop resolved = ShenandoahBarrierSet::resolve_forwarded_not_null(obj);
-        if (oopDesc::unsafe_equals(resolved, obj)) {
-          bool evac;
-          _heap->evacuate_object(obj, _thread, evac);
-        }
-      }
-    }
-  }
-
-public:
-  void do_oop(oop* p) {
-    do_oop_work(p);
-  }
-  void do_oop(narrowOop* p) {
-    do_oop_work(p);
-  }
-};
-
 class ShenandoahParallelEvacuateRegionObjectClosure : public ObjectClosure {
 private:
   ShenandoahHeap* const _heap;
